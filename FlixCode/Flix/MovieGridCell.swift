257d01e3fc3e9b5b55be961c32584eccaafb2742
MubdeMohashin <96319742+MubdeMohashin@users.noreply.github.com>
2022-02-23 20:35:03 -0500

    Flix

---
+++ FlixCode/Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Mubde Mohashin on 2/23/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
